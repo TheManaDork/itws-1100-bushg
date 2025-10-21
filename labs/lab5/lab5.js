f6c9302cd0edd7c16f38c984bec3ef41951af9f4
TheManaDork <zeldagamer36@gmail.com>
2025-10-20 21:43:57 -0400

    nearly there!

--- labs/lab5/lab5.js
+++ labs/lab5/lab5.js
@@ -4,40 +4,21 @@
 function validate(formObj) {
    // put your validation code here
    // it will be a series of if statements
-
-   // if(formObj.firstName.value == "") {
-   //    alert("You must enter a first name");
-   //    formObj.firstName.focus();
-   //    return false;
-   // }
-   // if(formObj.lastName.value == "") {
-   //    alert("You must enter a last name");
-   //    formObj.lastName.focus();
-   //    return false;
-   // }
-   // if(formObj.title.value == "") {
-   //    alert("You must enter a title");
-   //    formObj.title.focus();
-   //    return false;
-   // }
-   // if(formObj.org.value == "") {
-   //    alert("You must enter an organization");
-   //    formObj.org.focus();
-   //    return false;
-   // }
-   // if(formObj.pseudonym.value == "") {
-   //    alert("You must enter a nickname");
-   //    formObj.pseudonym.focus();
-   //    return false;
-   // }
+   var formObj = document.getElementById("addForm");
+   var error = false;
+   for(var e = 1; e < formObj.elements.length-1; e++) {
+      if(formObj.elements[e].value == "" || formObj.elements[e].value == "Please enter your " + document.querySelector('label[for='+formObj.elements[e].id+']').innerHTML) {
+         alert("The " + document.querySelector('label[for='+formObj.elements[e].id+']').innerHTML + " cannot be empty");
+         error = true;
+      }
+   }
+   if(error) {
+      return false;
+   }
    var formData = "";
    for(var e = 1; e < formObj.elements.length-1; e++) {
       formData = formData + "<br>" + formObj.elements[e].value;
-   //    alert(formObj.elements[e].name + " : " + formObj.elements[e].value);
-   // }
-   // console.log("Form has " + formObj.elements.length + " elements.");
-   // for (var e = 0; e < formObj.elements.length; e++) {
-      console.log("Element " + e + ":", formObj.elements[e]);
+      console.log("Element " + e + ":", formObj.elements[e].value);
    }
    document.getElementById("results").innerHTML = formData;
    // event.preventDefault();
@@ -45,22 +26,23 @@ function validate(formObj) {
 }
 
 
-function clearPlaceholderText(textbox) {
-   console.log(textbox);
-   console.log(textbox.innerHTML);
-   if(textbox.innerHTML == "Please enter your comments") {
-      textbox.innerHTML = "";
-   }
-}
+document.addEventListener("click", function() {
+   var formObj = document.getElementById("addForm");
+   var labels = document.getElementsByTagName("label");
+   for(var e = 1; e < formObj.elements.length-1; e++) {
+      if(formObj.elements[e].value == "" && document.activeElement.id != formObj.elements[e].id) {
+         formObj.elements[e].value = "Please enter your " + document.querySelector('label[for='+formObj.elements[e].id+']').innerHTML;
+         // console.log("text restored");
+      } else if(formObj.elements[e].value == "Please enter your " + document.querySelector('label[for='+formObj.elements[e].id+']').innerHTML && document.activeElement.id == formObj.elements[e].id) {
+         formObj.elements[e].value = "";
+         // console.log("text cleared");
+      }
 
-document.addEventListener("click", restorePlaceholderText());
-function restorePlaceholderText() {
-   textbox = document.getElementById("comments");
-   console.log("textbox: " + textbox);
-   // if(textbox.innerHTML == "") {
-      // textbox.innerHTML = "Please enter your comments";
-   // }
-}
+      if(formObj.elements[e].id == document.activeElement.id) {
+         console.log(formObj.elements[e].value == "Please enter your " + document.querySelector('label[for='+formObj.elements[e].id+']').innerHTML);
+      }
+   }
+});
 
 
 
