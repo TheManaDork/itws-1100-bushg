f08c1836e9726baa51ce11680f83bf8583074b0d
TheManaDork <zeldagamer36@gmail.com>
2025-11-13 15:33:02 -0500

    lil shifts

--- labs/lab8/menuBar.js
+++ labs/lab8/menuBar.js
@@ -1,8 +1,8 @@
 
 
 $(document).ready(function() {
-  $.ajax({url: "../resources/menuItems.json", success: function(result) {
-  	alert(result);
+  $.ajax({url: "../iit/resources/menuItems.json", success: function(result) {
+  	alert("result");
   }});
 
   document.getElementById("nav-bar").innerHTML += '<a class="Button" href="#">Hi</a>';
