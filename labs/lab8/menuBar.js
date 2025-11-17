ec34b7d4b93a2f0638ffbf1ddea3a15f632f30d7
TheManaDork <zeldagamer36@gmail.com>
2025-11-17 17:41:53 -0500

    Update menuBar.js

--- labs/lab8/menuBar.js
+++ labs/lab8/menuBar.js
@@ -2,23 +2,23 @@
 
 $(document).ready(function() {
   // alert("doc loaded");
-  // $.ajax({
-  // 	type: "GET",
-  // 	url: "resources/menuItems.json",
-  // 	dataType: "json",
-  // 	success: function(responseData, status) {
-  // 	  var output = "";
-  // 	  alert("success " + responseData);
-  // 	  $.each(responseData.items, function(i, menuItem) {
-  // 	    output+= '<a class="button" href="' + menuItem.link + '">' + menuItem.title + '</a>';
-  // 	  });
-  // 	  document.getElementById("nav-bar").innerHTML += output;
-  //     alert("|"output+"|");
-  //     // hi
-  // 	}, error: function(msg) {
-  // 		alert("darnit: " + msg.status + "\n" + msg.statusText);
-  // 	}
-  // });
+  $.ajax({
+  	type: "GET",
+  	url: "resources/menuItems.json",
+  	dataType: "json",
+  	success: function(responseData, status) {
+  	  var output = "";
+  	  alert("success " + responseData);
+  	  $.each(responseData.items, function(i, menuItem) {
+  	    output+= '<a class="button" href="' + menuItem.link + '">' + menuItem.title + '</a>';
+  	  });
+  	  document.getElementById("nav-bar").innerHTML += output;
+      alert("|"output+"|");
+      // hi
+  	}, error: function(msg) {
+  		alert("darnit: " + msg.status + "\n" + msg.statusText);
+  	}
+  });
 
   // alert("past Ajax");
 });
