e0dabfc6be78138196820552eca44e9c0dfe069d
TheManaDork <zeldagamer36@gmail.com>
2025-11-17 19:17:16 -0500

    Update menuBar.js

--- labs/lab8/menuBar.js
+++ labs/lab8/menuBar.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   	dataType: "json",
   	success: function(responseData, status) {
   	  var output = "";
-  	  alert("success " + responseData);
+  	  // alert("success " + responseData);
       let menuItem = responseData[document.title]; // I implemented this for my home page as well, I am currently determining what json to load based on page title.
       if(!menuItem) {
         alert("error: menuItem array emptry");
@@ -26,6 +26,7 @@ $(document).ready(function() {
 
 
   document.getElementById('monty').addEventListener("click", function() {
+    alert(this.src.substring(this.src.lastIndexOf('/'), this.src[this.src.length-1]));
     if(this.src.substring(this.src.lastIndexOf('/'), this.src[this.src.length-1]) == "mont1.jpg") {
       this.src = this.src.substring(0,this.src.lastIndexOf('/')+1) + "mont2.jpg";
     } else {
